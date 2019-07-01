eac04091869c3e7d6b1646a920c6bcac097b5780
Alexander Ivanov <33380175+xanderhub@users.noreply.github.com>
2019-07-01 13:59:20 +0300

    add h2-console visibility

--- src/main/java/com/xanderhub/flightapp/service/FlightService.java
+++ src/main/java/com/xanderhub/flightapp/service/FlightService.java
@@ -8,8 +8,6 @@ import com.xanderhub.flightapp.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.zip.CheckedOutputStream;
-
 @Service
 public class FlightService {
     private final TicketRepository ticketRepository;
