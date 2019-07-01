01b72946b053ba25f225b9fcfc3955632c199bbb
Alexander Ivanov <33380175+xanderhub@users.noreply.github.com>
2019-07-01 10:44:28 +0300

    Initial commit

---
+++ src/test/java/com/xanderhub/flightapp/FlightAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xanderhub.flightapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlightAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
